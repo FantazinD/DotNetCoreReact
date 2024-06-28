06f81ebe5697e69ccaa4166da039e130f3685b19
Fantazindy <paulo.pete.k.razon@gmail.com>
2024-06-28 22:08:00 +0800

    rollback original axios string

--- clientapp/api.tsx
+++ clientapp/api.tsx
@@ -7,7 +7,9 @@ interface SearchResponse {
 
 export const searchCompanies = async (query: string) => {
     try {
-        const data = await axios.get<SearchResponse>(`www.google.com`);
+        const data = await axios.get<SearchResponse>(
+            `https://financialmodelingprep.com/api/v3/search?query=${query}&limit=10&exchange=NASDAQ&apikey=${process.env.REACT_APP_API_KEY}`
+        );
         return data;
     } catch (error) {
         if (axios.isAxiosError(error)) {
