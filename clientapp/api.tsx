8ae849c4f61909f1d594440561dafeaeb10aac1b
Fantazindy <paulo.pete.k.razon@gmail.com>
2024-06-28 22:07:28 +0800

    changed axios string into test website

--- clientapp/api.tsx
+++ clientapp/api.tsx
@@ -7,9 +7,7 @@ interface SearchResponse {
 
 export const searchCompanies = async (query: string) => {
     try {
-        const data = await axios.get<SearchResponse>(
-            `https://financialmodelingprep.com/api/v3/search?query=${query}&limit=10&exchange=NASDAQ&apikey=${process.env.REACT_APP_API_KEY}`
-        );
+        const data = await axios.get<SearchResponse>(`www.google.com`);
         return data;
     } catch (error) {
         if (axios.isAxiosError(error)) {
