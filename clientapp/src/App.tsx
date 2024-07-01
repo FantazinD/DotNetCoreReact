3d2f9de89272fdc41d547286744a4853a563e227
Fantazindy <paulo.pete.k.razon@gmail.com>
2024-07-01 22:44:14 +0800

    added type narrowing to onClick function on Search

--- clientapp/src/App.tsx
+++ clientapp/src/App.tsx
@@ -2,17 +2,26 @@ import { ChangeEvent, SyntheticEvent, useState } from "react";
 import "./App.css";
 import CardList from "./Components/CardList/CardList";
 import Search from "./Components/Search/Search";
+import { ICompanySearch } from "./company";
+import { searchCompanies } from "./api";
 
 function App() {
     const [search, setSearch] = useState<string>("");
+    const [searchResult, setSearchResult] = useState<ICompanySearch[]>([]);
+    const [serverError, setServerError] = useState<string>("");
 
     const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
         setSearch(e.target.value);
         console.log(e);
     };
 
-    const onClick = (e: SyntheticEvent) => {
-        console.log(e);
+    const onClick = async (e: SyntheticEvent) => {
+        const result = await searchCompanies(search);
+        if (typeof result === "string") {
+            setServerError(result);
+        } else if (Array.isArray(result.data)) {
+            setSearchResult(result.data);
+        }
     };
 
     return (
