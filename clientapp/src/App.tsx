7a6b6842234d4a950d6879835e457e8bb7134cf7
Fantazindy <paulo.pete.k.razon@gmail.com>
2024-07-01 22:55:01 +0800

    added conditional rendering on serverError

--- clientapp/src/App.tsx
+++ clientapp/src/App.tsx
@@ -21,12 +21,14 @@ function App() {
             setServerError(result);
         } else if (Array.isArray(result.data)) {
             setSearchResult(result.data);
+            setServerError("");
         }
     };
 
     return (
         <div className="App">
             <Search onClick={onClick} search={search} handleChange={handleChange} />
+            {serverError && <h1>{serverError}</h1>}
             <CardList />
         </div>
     );
