f172e4df24e7dc6f90320e2de057ef08d61d3935
Fantazindy <paulo.pete.k.razon@gmail.com>
2024-07-03 18:15:10 +0800

    added Hero and Navbar components to clientapp

--- clientapp/src/App.tsx
+++ clientapp/src/App.tsx
@@ -5,6 +5,8 @@ import Search from "./Components/Search/Search";
 import { ICompanySearch } from "./company";
 import { searchCompanies } from "./api";
 import ListPortfolio from "./Components/Portfolio/ListPortfolio/ListPortfolio";
+import Navbar from "./Components/Navbar/Navbar";
+import Hero from "./Components/Hero/Hero";
 
 function App() {
     const [search, setSearch] = useState<string>("");
@@ -46,6 +48,7 @@ function App() {
 
     return (
         <div className="App">
+            <Navbar />
             <Search onSearchSubmit={onSearchSubmit} search={search} handleSearchChange={handleSearchChange} />
             <ListPortfolio portfolioValues={portfolioValues} onPortfolioDelete={onPortfolioDelete} />
             <CardList searchResults={searchResult} onPortfolioCreate={onPortfolioCreate} />
