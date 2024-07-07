ef62ae1a874ae815a48cac0e34681193750f4f0a
Fantazindy <paulo.pete.k.razon@gmail.com>
2024-07-07 20:03:51 +0800

    added Cashflow statement component to clientapp

---
+++ clientapp/src/Components/CashflowStatement/CashflowStatement.tsx
@@ -0,0 +1,10 @@
+import "./CashflowStatement.css";
+import React from "react";
+
+interface IProps {}
+
+const CashflowStatement = ({}: IProps) => {
+    return <div>CashflowStatement</div>;
+};
+
+export default CashflowStatement;
