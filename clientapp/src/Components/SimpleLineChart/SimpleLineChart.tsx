e83f58bd9ee7ac4f69ea4ec5e5d12eb25a710d63
Fantazindy <paulo.pete.k.razon@gmail.com>
2024-07-07 19:36:34 +0800

    added SimpleLineChart component to clientapp

---
+++ clientapp/src/Components/SimpleLineChart/SimpleLineChart.tsx
@@ -0,0 +1,10 @@
+import "./SimpleLineChart.css";
+import React from "react";
+
+interface IProps {}
+
+const SimpleLineChart = ({}: IProps) => {
+    return <div>SimpleLineChart</div>;
+};
+
+export default SimpleLineChart;
