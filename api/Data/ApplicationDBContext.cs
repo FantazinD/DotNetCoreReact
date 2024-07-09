6b5265801803025f98db8635cac6a7a8fd8595f6
Fantazindy <paulo.pete.k.razon@gmail.com>
2024-07-10 04:35:04 +0800

    created database by ef migration

--- api/Data/ApplicationDBContext.cs
+++ api/Data/ApplicationDBContext.cs
@@ -9,7 +9,7 @@ namespace api.Data
 {
     public class ApplicationDBContext: DbContext
     {
-        public ApplicationDBContext(DbContextOptions dbContextOptions) : base(dbContextOptions)
+        public ApplicationDBContext(DbContextOptions<ApplicationDBContext> dbContextOptions) : base(dbContextOptions)
         {
             
         }
