10ceaca5380ff6e082c53535cbf8822ef757d014
Fantazindy <paulo.pete.k.razon@gmail.com>
2024-07-10 23:30:00 +0800

    added CommentController to api

---
+++ api/Controllers/CommentController.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using api.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace api.Controllers
+{
+    public class CommentController : ControllerBase
+    {
+        private readonly ApplicationDBContext _context;
+        public CommentController(ApplicationDBContext context)
+        {
+            _context = context;
+        }
+    }
+}
