3d11bcede1b37cf8f6986d2c43f100047a25cd31
Fantazindy <paulo.pete.k.razon@gmail.com>
2024-07-16 09:47:13 +0800

    added login and register api to clientapp

--- clientapp/src/Services/AuthService.tsx
+++ clientapp/src/Services/AuthService.tsx
@@ -1,11 +1,26 @@
 import axios from "axios";
 import { handleError } from "../Helpers/ErrorHandler";
+import { UserProfileToken } from "../Models/User";
 
 const api = "http://localhost:5167/api";
 
 export const loginAPI = async (username: string, password: string) => {
     try {
-        const data = await axios.post<any>(`apiaccount/login"`, {
+        const data = await axios.post<UserProfileToken>(api + `account/login`, {
+            username: username,
+            password: password,
+        });
+
+        return data;
+    } catch (error) {
+        handleError(error);
+    }
+};
+
+export const registerAPI = async (username: string, email: string, password: string) => {
+    try {
+        const data = await axios.post<UserProfileToken>(api + `account/register"`, {
+            email: email,
             username: username,
             password: password,
         });
