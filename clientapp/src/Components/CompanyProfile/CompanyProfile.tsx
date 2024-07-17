965b09a467164929ac2b31c0c0d4e6430f369b7d
Fantazindy <paulo.pete.k.razon@gmail.com>
2024-07-18 03:26:44 +0800

    added StockComment component in clientapp

--- clientapp/src/Components/CompanyProfile/CompanyProfile.tsx
+++ clientapp/src/Components/CompanyProfile/CompanyProfile.tsx
@@ -6,6 +6,7 @@ import { getKeyMetrics } from "../../api";
 import RatioList from "../RatioList/RatioList";
 import Spinner from "../Spinner/Spinner";
 import { formatLargeNonMonetaryNumber, formatRatio } from "../../Helpers/NumberFormatting";
+import StockComment from "../StockComment/StockComment";
 
 interface IProps {}
 
@@ -80,6 +81,7 @@ const CompanyProfile = ({}: IProps) => {
             {companyData ? (
                 <>
                     <RatioList config={tableConfig} data={companyData} />
+                    <StockComment stockSymbol={ticker} />
                 </>
             ) : (
                 <Spinner />
---
+++ clientapp/src/Components/StockComment/StockComment.tsx
@@ -0,0 +1,12 @@
+import "./StockComment.css";
+import React from "react";
+
+interface IProps {
+    stockSymbol: string;
+}
+
+const StockComment = ({ stockSymbol }: IProps) => {
+    return <div>StockComment</div>;
+};
+
+export default StockComment;
