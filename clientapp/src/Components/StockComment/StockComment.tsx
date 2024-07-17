6629fe9b20bd88b428af0bb4de05bff72415ee19
Fantazindy <paulo.pete.k.razon@gmail.com>
2024-07-18 03:46:47 +0800

    completed StockComent component logic

--- clientapp/src/Components/StockComment/StockComment.tsx
+++ clientapp/src/Components/StockComment/StockComment.tsx
@@ -1,13 +1,32 @@
 import "./StockComment.css";
 import React from "react";
 import StockCommentForm from "./StockCommentForm/StockCommentForm";
+import { commentPostAPI } from "../../Services/CommentService";
+import { toast } from "react-toastify";
 
 interface IProps {
     stockSymbol: string;
 }
 
+type CommentFormInputs = {
+    title: string;
+    content: string;
+};
+
 const StockComment = ({ stockSymbol }: IProps) => {
-    return <StockCommentForm stockSymbol={stockSymbol} handleComment={() => {}} />;
+    const handleComment = (e: CommentFormInputs) => {
+        commentPostAPI(e.title, e.content, stockSymbol)
+            .then((res) => {
+                if (res) {
+                    toast.success("Comment created successfully!");
+                }
+            })
+            .catch((e) => {
+                toast.warning(e);
+            });
+    };
+
+    return <StockCommentForm stockSymbol={stockSymbol} handleComment={handleComment} />;
 };
 
 export default StockComment;
