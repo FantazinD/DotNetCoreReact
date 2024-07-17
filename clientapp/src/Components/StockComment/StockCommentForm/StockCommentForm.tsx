269ba91d842602ccec1e185532799436d99de118
Fantazindy <paulo.pete.k.razon@gmail.com>
2024-07-18 03:28:50 +0800

    added StockCommentForm component in clientapp

---
+++ clientapp/src/Components/StockComment/StockCommentForm/StockCommentForm.tsx
@@ -0,0 +1,10 @@
+import "./StockComment.css";
+import React from "react";
+
+interface IProps {}
+
+const StockCommentForm = ({}: IProps) => {
+    return <div>StockCommentForm</div>;
+};
+
+export default StockCommentForm;
