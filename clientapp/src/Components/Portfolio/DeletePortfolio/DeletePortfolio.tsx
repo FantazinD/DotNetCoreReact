a377e5d13ad6a84b90c3137ac9867624625ecb85
Fantazindy <paulo.pete.k.razon@gmail.com>
2024-11-06 05:12:24 +0800

    refactored DeletePortfolio component to address warning

--- clientapp/src/Components/Portfolio/DeletePortfolio/DeletePortfolio.tsx
+++ clientapp/src/Components/Portfolio/DeletePortfolio/DeletePortfolio.tsx
@@ -10,7 +10,7 @@ const DeletePortfolio = ({ onPortfolioDelete, portfolioValue }: IProps) => {
     return (
         <div>
             <form onSubmit={onPortfolioDelete}>
-                <input hidden={true} value={portfolioValue} />
+                <input hidden={true} defaultValue={portfolioValue} />
                 <button className="block w-full py-3 text-white duration-200 border-2 rounded-lg bg-red-500 hover:text-red-500 hover:bg-white border-red-500">
                     X
                 </button>
