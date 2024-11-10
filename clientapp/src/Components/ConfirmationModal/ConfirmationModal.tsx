680f21c7d41aa532cdbba88b551e8b721322a3c5
Fantazindy <paulo.pete.k.razon@gmail.com>
2024-11-10 17:04:28 +0800

    added ConfirmationModal component

---
+++ clientapp/src/Components/ConfirmationModal/ConfirmationModal.tsx
@@ -0,0 +1,12 @@
+import "./ConfirmationModal.css";
+import React from "react";
+
+type IProps = {
+    onClickConfirm: () => void;
+};
+
+const ConfirmationModal = ({ onClickConfirm }: IProps) => {
+    return <div>ConfirmationModal</div>;
+};
+
+export default ConfirmationModal;
