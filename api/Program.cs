1fb83c93012b8580f5166b08d527d81e26319746
Fantazindy <paulo.pete.k.razon@gmail.com>
2024-11-16 05:15:22 +0800

    removed duplicate code

--- api/Program.cs
+++ api/Program.cs
@@ -16,7 +16,6 @@ var builder = WebApplication.CreateBuilder(args);
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
 
 builder.Services.AddSwaggerGen(option =>
 {
