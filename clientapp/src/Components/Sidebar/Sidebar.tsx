51ba56c57746c575d95ba4bf89d793e7e7114d67
Fantazindy <paulo.pete.k.razon@gmail.com>
2024-11-18 10:59:53 +0800

    sidebar z index to 20

--- clientapp/src/Components/Sidebar/Sidebar.tsx
+++ clientapp/src/Components/Sidebar/Sidebar.tsx
@@ -8,7 +8,7 @@ interface IProps {}
 
 const Sidebar = ({}: IProps) => {
     return (
-        <nav className="block py-4 px-6 top-0 bottom-0 w-64 bg-white shadow-xl left-0 absolute flex-row flex-nowrap md:z-10 z-9999 transition-all duration-300 ease-in-out transform md:translate-x-0 -translate-x-full">
+        <nav className="block py-4 px-6 top-0 bottom-0 w-64 bg-white shadow-xl left-0 absolute flex-row flex-nowrap md:z-10 z-20 transition-all duration-300 ease-in-out transform md:translate-x-0 -translate-x-full">
             <div className="flex-col min-h-full px-0 flex flex-wrap items-center justify-between w-full mx-auto overflow-y-auto overflow-x-hidden">
                 <div className="flex bg-white flex-col items-stretch opacity-100 relative mt-4 overflow-y-auto overflow-x-hidden h-auto z-40 items-center flex-1 rounded w-full">
                     <div className="md:flex-col md:min-w-full flex flex-col list-none">
