fd5ea89512fd073b8dfc63b436ca70d3ab5a8363
Fantazindy <paulo.pete.k.razon@gmail.com>
2025-01-28 16:13:39 +0800

    removed comments

--- api/Interfaces/IFMPService.cs
+++ api/Interfaces/IFMPService.cs
@@ -2,7 +2,6 @@ using api.Models;
 
 namespace api.Interfaces
 {
-    //FMP = Financial Modeling Prep
     public interface IFMPService
     {
         Task<Stock> FindStockBySymbolAsync(string stockSymbol);
