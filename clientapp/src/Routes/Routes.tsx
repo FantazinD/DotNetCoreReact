c2e64179b1b2eeefc959f66c4a4c5dec8febc74d
Fantazindy <paulo.pete.k.razon@gmail.com>
2025-02-15 17:15:22 +0800

    update imports

--- clientapp/src/Routes/Routes.tsx
+++ clientapp/src/Routes/Routes.tsx
@@ -1,17 +1,17 @@
-import { createBrowserRouter } from "react-router-dom";
-import HomePage from "../Pages/HomePage/HomePage";
-import App from "../App";
-import SearchPage from "../Pages/SearchPage/SearchPage";
-import CompanyPage from "../Pages/CompanyPage/CompanyPage";
-import CompanyProfile from "../Components/CompanyProfile/CompanyProfile";
-import IncomeStatement from "../Components/IncomeStatement/IncomeStatement";
-import DesignPage from "../Pages/DesignPage/DesignPage";
-import BalanceSheet from "../Components/BalanceSheet/BalanceSheet";
 import HistoricalDividend from "../Components/HistoricalDividend/HistoricalDividend";
 import CashFlowStatement from "../Components/CashFlowStatement/CashFlowStatement";
-import LoginPage from "../Pages/LoginPage/LoginPage";
+import IncomeStatement from "../Components/IncomeStatement/IncomeStatement";
+import CompanyProfile from "../Components/CompanyProfile/CompanyProfile";
+import BalanceSheet from "../Components/BalanceSheet/BalanceSheet";
 import RegisterPage from "../Pages/RegisterPage/RegisterPage";
+import CompanyPage from "../Pages/CompanyPage/CompanyPage";
+import SearchPage from "../Pages/SearchPage/SearchPage";
+import DesignPage from "../Pages/DesignPage/DesignPage";
+import { createBrowserRouter } from "react-router-dom";
+import LoginPage from "../Pages/LoginPage/LoginPage";
+import HomePage from "../Pages/HomePage/HomePage";
 import ProtectedRoute from "./ProtectedRoute";
+import App from "../App";
 
 export const router = createBrowserRouter([
     {
