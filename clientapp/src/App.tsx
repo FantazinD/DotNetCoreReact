f5b6b27b3ea0b8ea10e110943674a2925aac06bf
Fantazindy <paulo.pete.k.razon@gmail.com>
2025-02-23 22:45:48 +0800

    updated imports

--- clientapp/src/App.tsx
+++ clientapp/src/App.tsx
@@ -1,9 +1,9 @@
+import { UserProvider } from "./Context/useAuth";
+import { ToastContainer } from "react-toastify";
+import Navbar from "./Components/Navbar/Navbar";
+import "react-toastify/dist/ReactToastify.css";
 import { Outlet } from "react-router";
 import "./App.css";
-import "react-toastify/dist/ReactToastify.css";
-import Navbar from "./Components/Navbar/Navbar";
-import { ToastContainer } from "react-toastify";
-import { UserProvider } from "./Context/useAuth";
 
 function App() {
     return (
